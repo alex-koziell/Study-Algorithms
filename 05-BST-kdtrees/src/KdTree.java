ca77a22a6d14629d6f80aa192568315dac4bb9df
Alex Koziell <alexkoziell@gmail.com>
2020-04-08 22:29:48 +0100

    Working Kd-tree and PointSET implementations.

--- 05-BST-kdtrees/src/KdTree.java
+++ 05-BST-kdtrees/src/KdTree.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.*;
+import org.w3c.dom.css.Rect;
 
 import java.util.ArrayList;
 
@@ -14,7 +15,7 @@ public class KdTree {
     }
 
     // is the set empty?
-    public boolean isEmpty() { return (root == null); }
+    public boolean isEmpty() { return (numNodes == 0); }
 
     // number of points in the set
     public int size() { return numNodes; }
@@ -22,75 +23,35 @@ public class KdTree {
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         if (p == null) throw new IllegalArgumentException("Null argument!");
-        root = insert(root, p,true, 0, 0, p.x(), 1);
-
-//        if (root == null) {
-//            // empty tree
-//            root = new Node(p);
-//        } else {
-//            // search for point
-//            boolean compareX = true;
-//            Node currentNode = root;
-//            Node nextNode;
-//
-//            while (true) {
-//                // conditionally compare x or y coordinates
-//                if (compareX ? (p.x() < currentNode.p.x()) : (p.y() < currentNode.p.y())) {
-//                    nextNode = currentNode.lb;
-//                    // if we've reached a leaf
-//                    if (nextNode == null) {
-//                        currentNode.lb = new Node(p);
-//                        break;
-//                    }
-//                } else {
-//                    nextNode = currentNode.rt;
-//                    if (nextNode == null) {
-//                        currentNode.rt = new Node(p);
-//                        break;
-//                    }
-//                }
-//
-//                currentNode = nextNode;
-//                // switch x/y comparison at each level
-//                compareX = !compareX;
-//            }
-//        }
+        root = insert(root, p, true, 0, 0, 1, 1);
     }
 
 
     private Node insert(Node node, Point2D p, boolean compareX, double minX, double minY, double maxX, double maxY) {
         if (node == null) {
             ++numNodes;
+
             // create a new leaf
-            return new Node(p, new RectHV(minX, minY, maxX, maxY));
+            RectHV rect;
+            if (compareX) {
+                rect = new RectHV(p.x(), minY, p.x(), maxY); // create vertical line
+            }
+            else rect = new RectHV(minX, p.y(), maxX, p.y()); // horizontal line
+
+            System.out.println(rect.toString());
+            return new Node(p, rect);
         }
         // if we find the point already in the tree
         if (node.p.equals(p)) return node;
 
-        boolean cmp = compareX ? (p.x() < node.p.x()) : (p.y() < node.p.y());
-        if (cmp) {
-            // new point to left of parent
-            if (compareX) {
-                maxX = node.p.x();
-                maxY = p.y();
-            // new point below parent
-            } else {
-                maxX = p.x();
-                maxY = node.p.y();
-            }
-            node.lb = insert(node.lb, p, !compareX, minX, minY, maxX, maxY);
-        }
-        else {
-            // new point to right of parent
-            if (compareX) {
-                minX = node.p.x();
-                minY = p.y();
-            // new point above parent
-            } else {
-                minX = p.x();
-                minY = node.p.y();
-            }
-            node.rt = insert(node.rt, p, !compareX, minX, minY, maxX, maxY);
+        if (compareX) {
+            // current node vertical line; next one is horizontal
+            if (p.x() < node.p.x()) node.lb = insert(node.lb, p, false, minX, minY, node.p.x(), maxY);
+            else node.rt = insert(node.rt, p, false, node.p.x(), minY, maxX, maxY);
+        } else {
+            // current node horizontal line
+            if (p.y() < node.p.y()) node.lb = insert(node.lb, p, true, minX, minY, maxX, node.p.y());
+            else node.rt = insert(node.rt, p, true, minX, node.p.y(), maxX, maxY);
         }
 
         return node;
@@ -113,11 +74,23 @@ public class KdTree {
 
     // draw all points to standard draw
     public void draw() {
-        int count = 0;
-        while (count < numNodes) {
+        ArrayList<Node> allNodes = new ArrayList<>();
+        allNodes(root, allNodes);
 
+        for (Node node : allNodes) {
+            StdDraw.setPenColor(StdDraw.BLACK);
+            node.p.draw();
+            StdDraw.setPenColor(StdDraw.BLUE);
+            if (node.rect.xmin() == node.rect.xmax()) StdDraw.setPenColor(StdDraw.RED);
+            node.rect.draw();
         }
+    }
 
+    private void allNodes(Node node, ArrayList<Node> nodes) {
+        if (node == null) return;
+        nodes.add(node);
+        allNodes(node.lb, nodes);
+        allNodes(node.rt, nodes);
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -131,9 +104,7 @@ public class KdTree {
     }
 
     private void range(Node node, RectHV rect, ArrayList<Point2D> results) {
-        if (node == null) {
-            return;
-        }
+        if (node == null) return;
         // if this point in the rectangle, we might have more points on either side
         if (rect.contains(node.p)) {
             results.add(node.p);
@@ -141,14 +112,12 @@ public class KdTree {
             range(node.rt, rect, results);
             return;
         }
-        // if this point to left or below query rect, only points above or right might be in range
-        if ((node.p.x() < rect.xmin()) || (node.p.y() < rect.ymin())) {
-            range(node.rt, rect, results);
-        }
-        // vice versa for point right or above query rect
-        if ((node.p.x() > rect.xmax()) || (node.p.y() > rect.ymax())) {
-            range(node.rt, rect, results);
-        }
+
+        // if rectangle min bounds to the left or below, search left/bottom branch
+        if (rect.xmin() < node.p.x() || rect.ymin() < node.p.y()) range(node.lb, rect, results);
+        // if rectangle max bounds above and to the right, search right/top branch
+        if (node.p.x() < rect.xmax() || node.p.y() < rect.ymax()) range(node.rt, rect, results);
+
     }
 
 
@@ -187,6 +156,8 @@ public class KdTree {
         Node(Point2D p, RectHV rect) {
             this.p = p;
             this.rect = rect;
+            this.lb = null;
+            this.rt = null;
         }
     }
 
